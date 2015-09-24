0a4379920b2ebd9351b940038b758ccfa6a8d903
huangpeng817 <huangpeng817@qq.com>
2015-09-24 15:15:32 +0800

    系统管理完成模块中的所有实体并总结映射实体的技巧

--- src/cn/itcast/oa/domain/Department.java
+++ src/cn/itcast/oa/domain/Department.java
@@ -1,10 +1,18 @@
 package cn.itcast.oa.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Department {
 
-	private Long id;
+	private Long id; 
+	private Set<User> users = new HashSet<User>();
+	private Department parent;
+	private Set<Department> children = new HashSet<Department>();
+	
 	private String name;
 	private String description;
+	
 	public Long getId() {
 		return id;
 	}
@@ -22,5 +30,23 @@ public class Department {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public Department getParent() {
+		return parent;
+	}
+	public void setParent(Department parent) {
+		this.parent = parent;
+	}
+	public Set<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	public Set<Department> getChildren() {
+		return children;
+	}
+	public void setChildren(Set<Department> children) {
+		this.children = children;
 	}
 }
--- src/cn/itcast/oa/domain/Role.java
+++ src/cn/itcast/oa/domain/Role.java
@@ -1,10 +1,15 @@
 package cn.itcast.oa.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Role {
 
 	private Long id;
 	private String name;
 	private String description;
+	private Set<User> users = new HashSet<User>();
+	
 	public Long getId() {
 		return id;
 	}
@@ -22,5 +27,11 @@ public class Role {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public Set<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Set<User> users) {
+		this.users = users;
 	}
 }
